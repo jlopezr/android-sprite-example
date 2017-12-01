fa42e0c05932ff67f71ef001db9c67bb9f2a41c2
Juan López Rubio <j.lopez.r@gmail.com>
2017-12-01 13:14:55 +0100

    Game loop added

---
+++ app/src/main/java/android/edu4java/com/killthemall/GameView.java
@@ -0,0 +1,59 @@
+package android.edu4java.com.killthemall;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView {
+    private Bitmap bmp;
+    private SurfaceHolder holder;
+    private GameLoopThread gameLoopThread;
+    private int x = 0;
+
+    public GameView(Context context) {
+        super(context);
+        gameLoopThread = new GameLoopThread(this);
+        holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                gameLoopThread.setRunning(true);
+                gameLoopThread.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                boolean retry = true;
+                gameLoopThread.setRunning(false);
+                while(retry) {
+                    try {
+                        gameLoopThread.join();
+                        retry = false;
+                    } catch (InterruptedException e) {
+                        // DO NOTHING
+                    }
+                }
+            }
+        });
+        bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK);
+        if (x < getWidth() - bmp.getWidth()) {
+            x++;
+        } else {
+            x = 0;
+        }
+        canvas.drawBitmap(bmp, x, 10, null);
+    }
+}
---
+++ app/src/main/java/android/edu4java/com/killthemall/MainActivity.java
@@ -0,0 +1,17 @@
+package android.edu4java.com.killthemall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
+        //setContentView(R.layout.activity_main);
+
+    }
+}
