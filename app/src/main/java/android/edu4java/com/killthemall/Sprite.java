b7b360da605e3d68d71825b412184dd53a88a0c2
Juan López Rubio <j.lopez.r@gmail.com>
2017-12-01 14:00:41 +0100

    One animation per direction

--- app/src/main/java/android/edu4java/com/killthemall/Sprite.java
+++ app/src/main/java/android/edu4java/com/killthemall/Sprite.java
@@ -4,50 +4,59 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
+import java.util.Random;
+
 public class Sprite {
     private static final int BMP_ROWS = 4;
     private static final int BMP_COLUMNS = 3;
     private int x = 0;
     private int y = 0;
-    private int xSpeed = 5;
+    private int xSpeed = 0;
+    private int ySpeed = 0;
     private GameView gameView;
     private Bitmap bmp;
     private int currentFrame = 0;
     private int width;
     private int height;
 
+    // direction = 0 up, 1 left, 2 down, 3 right,
+    // animation = 3 back, 1 left, 0 front, 2 right
+    private static final int[] DIRECTION_TO_ANIMATION_MAP = { 3, 1, 0, 2 };
+
     public Sprite(GameView gameView, Bitmap bmp) {
         this.gameView=gameView;
         this.bmp=bmp;
         this.width = bmp.getWidth() / BMP_COLUMNS;
         this.height = bmp.getHeight() / BMP_ROWS;
+        Random rnd = new Random();
+        xSpeed = rnd.nextInt(10)-5;
+        ySpeed = rnd.nextInt(10)-5;
     }
 
     private void update() {
-        if (x > gameView.getWidth() - width - xSpeed) {
-            xSpeed = -5;
-        }
-        if (x + xSpeed< 0) {
-            xSpeed = 5;
+        if (x > gameView.getWidth() - width - xSpeed || x + xSpeed < 0) {
+            xSpeed = -xSpeed;
         }
         x = x + xSpeed;
-
-        if (x > gameView.getWidth() - width - xSpeed) {
-            xSpeed = -5;
+        if (y > gameView.getHeight() - height - ySpeed || y + ySpeed < 0) {
+            ySpeed = -ySpeed;
         }
-        if (x + xSpeed < 0) {
-            xSpeed = 5;
-        }
-        x = x + xSpeed;
+        y = y + ySpeed;
         currentFrame = ++currentFrame % BMP_COLUMNS;
     }
 
     public void onDraw(Canvas canvas) {
         update();
         int srcX = currentFrame * width;
-        int srcY = 1 * height;
+        int srcY = getAnimationRow() * height;
         Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
         Rect dst = new Rect(x, y, x + width, y + height);
         canvas.drawBitmap(bmp, src, dst, null);
+    }
+
+    private int getAnimationRow() {
+        double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+        int direction = (int) Math.round(dirDouble) % BMP_ROWS;
+        return DIRECTION_TO_ANIMATION_MAP[direction];
     }
 }
